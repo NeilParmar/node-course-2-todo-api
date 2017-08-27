f3a431afba71bd7a2a076d4f066f15ff02a07727
Neil <>
2017-08-27 14:21:24 -0400

    Add POST /todos route and refactor mongoose

---
+++ playground/archieve.js
@@ -0,0 +1,32 @@
+//
+//
+// // var otherTodo = new Todo({
+// //   text: 'Feed the dog',
+// //   completed: true,
+// //   completedAt: 124
+// //
+// // });
+//
+// // var otherTodo = new Todo({
+// //   text: '  Edit this video  '
+// // });
+// //
+// // otherTodo.save().then((doc) => {
+// //   console.log(JSON.stringify(doc, undefined, 2));
+// // }, (e) => {
+// //   console.log('Unable to save todo\n',JSON.stringify(e,undefined,2));
+// // })
+//
+// //User
+//
+//
+// 
+// var user = new User({
+//   email: 'b'
+// });
+//
+// user.save().then((doc) => {
+//   console.log('Data entered successfully\n',JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save\n',JSON.stringify(e, undefined, 2));
+// });
